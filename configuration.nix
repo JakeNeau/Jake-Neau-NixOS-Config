2b122b98ffc49ff5459cf9bf0e35c6caf2123b39
Jake Neau <jakeneau@proton.me>
2025-10-01 19:17:53 -0500

    Start tracking hardware configuration again because apparently that breaks things

--- configuration.nix
+++ configuration.nix
@@ -1,7 +1,3 @@
-# Edit this configuration file to define what should be installed on
-# your system.  Help is available in the configuration.nix(5) man page
-# and in the NixOS manual (accessible by running ‘nixos-help’).
-
 { config, pkgs, lib, ... }:
 
 {
@@ -87,10 +83,6 @@
     LC_TIME = "en_US.UTF-8";
   };
 
-  # Enable the X11 windowing system.
-  # You can disable this if you're only using the Wayland session.
-  services.xserver.enable = true;
-
   # Compositor: displays the desktop
   programs.hyprland.enable = true;
 
@@ -130,10 +122,7 @@
     jack.enable = true;
   };
 
-  # Enable touchpad support (enabled default in most desktopManager).
-  # services.xserver.libinput.enable = true;
-
-  # Define a user account. Don't forget to set a password with ‘passwd’.
+  # User accounts
   users.mutableUsers= false;
   users.users.jakeneau = {
     isNormalUser = true;
@@ -169,7 +158,7 @@
     };
   };
 
-  # Declarative Neovim distrobution
+  # Declarative Neovim distribution
   programs.nvf = {
     enable = true;
     defaultEditor = true;
@@ -201,8 +190,7 @@
   };
 
 
-  # List packages installed in system profile. To search, run:
-  # $ nix search wget
+  # List packages installed in system profile
   environment.systemPackages = with pkgs; [
     audacity                              # Audio recording program
     bitwarden-desktop                     # Password manager for all devices
@@ -262,18 +250,34 @@
     enable = true;
     shellAliases = {
       # NixOS Aliases
-      nf = "sudoedit /etc/nixos/flake.nix";
-      nc = "sudoedit /etc/nixos/configuration.nix";
-      nh = "$EDITOR /etc/nixos/users/$USER/home.nix";
-      nr = "sudo nix flake update --flake /etc/nixos; sudo nixos-rebuild switch --upgrade --flake /etc/nixos";
+      nf = "git -C /etc/nixos pull; sudoedit /etc/nixos/flake.nix";
+      nc = "git -C /etc/nixos pull; sudoedit /etc/nixos/configuration.nix";
+      nh = "git -C /etc/nixos pull; $EDITOR /etc/nixos/users/$USER/home.nix";
       ng = "sudo nix-collect-garbage --delete-old";
-      ns = "sops /etc/nixos/secrets/secrets.yaml";
+      ns = "git -C /etc/nixos pull; sops /etc/nixos/secrets/secrets.yaml";
 
       # General Command Aliases
-      l = "eza -alh --git-repos --git";
+      ls = "eza";
       r = "sudo systemctl reboot";
       s = "sudo systemctl poweroff";
     };
+    shellInit = ''
+      function nr
+        if test (count $argv) -eq 0
+          git -C /etc/nixos pull; sudo nix flake update --flake /etc/nixos; sudo nixos-rebuild switch --upgrade --flake /etc/nixos; git -C /etc/nixos add *; git -C /etc/nixos commit --amend --no-edit; git -C /etc/nixos push --force-with-lease
+        else 
+          git -C /etc/nixos pull; sudo nix flake update --flake /etc/nixos; sudo nixos-rebuild switch --upgrade --flake /etc/nixos; git -C /etc/nixos add *; git -C /etc/nixos commit -m $argv; git -C /etc/nixos push
+        end
+      end
+
+      function l
+        if test (count $argv) -eq 0
+          eza -algh --git-repos --git
+        else
+          eza -algh --git-repos --git -T -L $argv
+        end
+      end
+    '';
   };
 
   # Style system with stylix
@@ -311,13 +315,6 @@
     };
   };
 
-  # Some programs need SUID wrappers, can be configured further or are
-  # started in user sessions.
-  # programs.mtr.enable = true;
-  # programs.gnupg.agent = {
-  #   enable = true;
-  #   enableSSHSupport = true;
-  # };
 
   # List services that you want to enable:
 
@@ -326,16 +323,8 @@
 
   # Open ports in the firewall.
   networking.firewall.allowedTCPPorts = [ 25565 ];
-  # networking.firewall.allowedUDPPorts = [ ... ];
-  # Or disable the firewall altogether.
-  # networking.firewall.enable = false;
 
-  # This value determines the NixOS release from which the default
-  # settings for stateful data, like file locations and database versions
-  # on your system were taken. It‘s perfectly fine and recommended to leave
-  # this value at the release version of the first install of this system.
-  # Before changing this value read the documentation for this option
-  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
-  system.stateVersion = "24.11"; # Did you read the comment?
-
+  # The first system version installed
+  # Do not change unless you know what you are doing, things will break
+  system.stateVersion = "24.11";
 }
