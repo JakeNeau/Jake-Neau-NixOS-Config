7713fb960c43c3ae0736ae0dbbc8c5395529213c
Jake Neau <jakeneau@proton.me>
2025-10-05 13:23:26 -0500

    Generation 428: Remove hyprpaper from hyprland startup because there is a systemd service

--- users/jakeneau/home.nix
+++ users/jakeneau/home.nix
@@ -58,9 +58,7 @@
         ];
       };
       # Enable programs on startup
-      exec-once = [
-        "hyprpaper"
-      ];
+      exec-once = [];
       # Monitors
       monitor = [
         "DP-1, 5120x1440@240, 0x0, 1"
