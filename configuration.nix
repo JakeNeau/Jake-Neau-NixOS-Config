9fea5f8725fe22f705de57baf13458c1b03f8ede
Jake Neau <jakeneau@proton.me>
2025-10-09 18:19:28 -0500

    Generation 449: Turn on the broken audio script

--- configuration.nix
+++ configuration.nix
@@ -143,14 +143,14 @@
           }
         ];
       };
-#      "92-low-latency" = {
-#        "context.properties" = {
-#          "default.clock.rate" = 48000;
-#          "default.clock.quantum" = 32;
-#          "default.clock.min-quantum" = 32;
-#          "default.clock.max-quantum" = 32;
-#        };
-#      };
+      "92-low-latency" = {
+        "context.properties" = {
+          "default.clock.rate" = 48000;
+          "default.clock.quantum" = 32;
+          "default.clock.min-quantum" = 32;
+          "default.clock.max-quantum" = 32;
+        };
+      };
     };
   };
   
