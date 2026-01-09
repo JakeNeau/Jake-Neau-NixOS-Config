1855362b26b31eccecb6cbc389a6d8dc382bbd92
Jake Neau <jakeneau@proton.me>
2026-01-09 14:21:56 -0600

    Generation 532: Run niri as a session

--- configuration.nix
+++ configuration.nix
@@ -233,7 +233,7 @@
     enable = true;
     settings = rec {
       initial_session = {
-        command = "${pkgs.niri}/bin/niri";
+        command = "${pkgs.niri}/bin/niri-session";
         user = "jakeneau";
       };
       default_session = initial_session;
