b7283a2970b0485ea65ef32348cb7c0b87749d5c
Jake Neau <jakeneau@proton.me>
2026-01-21 18:46:45 -0600

    Generation 538: Regular update

--- configuration.nix
+++ configuration.nix
@@ -284,6 +284,7 @@
     clipse                                # A cool clipboard manager
     eza                                   # A better version of ls written in rust
     fastfetch                             # Terminal program for displaying system info and flexing on arch users
+    freecad                               # An open source parametric 3D modeling program
     fishPlugins.colored-man-pages         # More interesting man pages
     fishPlugins.fish-bd                   # Go back directories with bd
     fishPlugins.fzf-fish                  # Search for multiple things with fzf
