6f93baff14bf42e172e7c1ed65b7c2d294a3141e
Jake Neau <jakeneau@proton.me>
2026-04-21 21:56:04 -0500

    Generation 662: Add figma

--- configuration.nix
+++ configuration.nix
@@ -579,6 +579,7 @@
     fastfetch # Terminal program for displaying system info and flexing on arch users
     ffmpeg # Video codec
     freecad # An open source parametric 3D modeling program
+    figma-linux # A web design tool
     fishPlugins.colored-man-pages # More interesting man pages
     fishPlugins.fish-bd # Go back directories with bd
     fishPlugins.fzf-fish # Search for multiple things with fzf
