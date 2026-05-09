25b23b14fb6ac82b9043dee454d41f4ba3f98e3e
Jake Neau <jakeneau@proton.me>
2026-05-09 16:17:43 -0500

    Generation 682: Add vim keymaps for navigating between splits

--- configuration.nix
+++ configuration.nix
@@ -560,6 +560,32 @@
         enable = true;
         gitStatus.enable = true;
       };
+
+      # ---------------
+      # Other keymaps
+      # ---------------
+      keymaps = [
+        {
+          key = "<C-h>";
+          mode = "n";
+          action = "<C-w>h";
+        }
+        {
+          key = "<C-j>";
+          mode = "n";
+          action = "<C-w>j";
+        }
+        {
+          key = "<C-k>";
+          mode = "n";
+          action = "<C-w>k";
+        }
+        {
+          key = "<C-l>";
+          mode = "n";
+          action = "<C-w>l";
+        }
+      ];
     };
   };
 
